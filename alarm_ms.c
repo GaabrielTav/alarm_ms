aee6bcba48fa86c9e8172de95cb40575af912791
Gabriel1 <gaabrieltav@gmail.com>
2025-02-04 22:13:34 -0300

    Alarmes Configurados

--- alarm_ms.c
+++ alarm_ms.c
@@ -1,8 +1,7 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
- 
-#define LED_BLUE_PIN 11    
+ #define LED_BLUE_PIN 11    
 #define LED_RED_PIN 12     
 #define LED_GREEN_PIN 13   
 #define BUTTON_PIN 5       
@@ -14,13 +13,12 @@
     STATE_ONE_ON,   
 } LedState;
 
- 
-LedState current_state = STATE_OFF;  
+ LedState current_state = STATE_OFF;  
 bool button_pressed = false;         
 bool timer_active = false;           
 
- 
-void initialize_components() {
+ void initialize_components() {
+
      gpio_init(LED_BLUE_PIN);
     gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
     gpio_init(LED_RED_PIN);
@@ -38,14 +36,17 @@ void initialize_components() {
 }
 
  int64_t turn_off_callback(alarm_id_t id, void *user_data) {
-     switch (current_state) {
+    // Altera o estado dos LEDs
+    switch (current_state) {
         case STATE_ALL_ON:
-            gpio_put(LED_BLUE_PIN, 0);  
+            gpio_put(LED_BLUE_PIN, 0); 
             current_state = STATE_TWO_ON;
+             add_alarm_in_ms(3000, turn_off_callback, NULL, false);
             break;
         case STATE_TWO_ON:
             gpio_put(LED_RED_PIN, 0);  
             current_state = STATE_ONE_ON;
+             add_alarm_in_ms(3000, turn_off_callback, NULL, false);
             break;
         case STATE_ONE_ON:
             gpio_put(LED_GREEN_PIN, 0);  
@@ -56,23 +57,19 @@ void initialize_components() {
             break;
     }
 
-     
-    return 0;
+     return 0;
 }
 
- 
-int main() {
-     
-    stdio_init_all();
+ int main() {
+     stdio_init_all();
 
-     
-    initialize_components();
+     initialize_components();
 
      while (true) {
          if (!gpio_get(BUTTON_PIN) && !button_pressed && !timer_active) {
             button_pressed = true;  
 
-            gpio_put(LED_BLUE_PIN, 1);
+             gpio_put(LED_BLUE_PIN, 1);
             gpio_put(LED_RED_PIN, 1);
             gpio_put(LED_GREEN_PIN, 1);
             current_state = STATE_ALL_ON;
@@ -82,7 +79,8 @@ int main() {
         }
 
          if (gpio_get(BUTTON_PIN) && button_pressed) {
-            button_pressed = false; 
+            button_pressed = false;  
+        }
 
          sleep_ms(10);
     }
